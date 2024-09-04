816fb3940a5c7944f1ef804481bafca6d4393613
JakesMD <71591438+JakesMD@users.noreply.github.com>
2024-09-04 08:41:22 +0200

    ✨ Upgrade platform client

--- packages/data/platform_client/lib/src/exceptions/share.dart
+++ packages/data/platform_client/lib/src/exceptions/share.dart
@@ -6,8 +6,8 @@ enum CShareException {
   /// The failure was unitentifiable.
   unknown;
 
-  factory CShareException.fromError(Object e) {
-    log(e.toString(), error: e, name: 'CShareException');
+  factory CShareException.fromError(Object e, StackTrace s) {
+    log(e.toString(), error: e, stackTrace: s, name: 'CShareException');
     return CShareException.unknown;
   }
 }
