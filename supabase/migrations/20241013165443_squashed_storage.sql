f5073cf0f6bd8031c71779650ba9f8297206590b
JakesMD <71591438+JakesMD@users.noreply.github.com>
2024-10-14 11:05:58 +0200

    ♻️ Update migrations

--- supabase/migrations/20241013165443_squashed_storage.sql
+++ supabase/migrations/20241013165443_squashed_storage.sql
@@ -1,3 +1,5 @@
+DROP POLICY IF EXISTS "Enable insert for authenticated users only" ON "storage"."objects";
+
 CREATE POLICY "Enable insert for authenticated users only" ON "storage"."objects" AS permissive
     FOR ALL TO authenticated
         USING (TRUE)
--- supabase/migrations/20241013165444_not_auto_diffed.sql
+++ supabase/migrations/20241013165444_not_auto_diffed.sql
@@ -6,10 +6,14 @@ GRANT usage ON SCHEMA "public" TO "supabase_auth_admin";
 
 REVOKE EXECUTE ON FUNCTION "public"."custom_access_token_hook" FROM authenticated, anon;
 
+DROP TRIGGER IF EXISTS on_auth_user_inserted ON auth.users;
+
 CREATE TRIGGER on_auth_user_inserted
     AFTER INSERT ON auth.users
     FOR EACH ROW
     EXECUTE FUNCTION handle_auth_user_insert();
+
+DROP TRIGGER IF EXISTS on_auth_user_updated ON auth.users;
 
 CREATE TRIGGER on_auth_user_updated
     AFTER UPDATE ON auth.users
